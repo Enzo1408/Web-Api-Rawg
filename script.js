9dc9502fddf4ba3246f55980e1a86d11d6485f1a
Enzo Cornaggia <enzo.cornaggia@etu.u-bordeaux.fr>
2023-01-31 22:56:21 +0100

    add some presentation

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ function catch_list_game() {
 }
 
 const wrapperGallery = document.querySelector('.wrapper-gallery');
+
 for (let i = 1; i <= 16; i++){
     // Injecte des div dans le HTML
     const gameElement = document.createElement('div');
-
     // Ajoute à chaque div la classe .game
     gameElement.classList.add('game');
-
     // Positionne chaque case dans la grid en utilisant grid-area CSS
     gameElement.style.gridArea = `game${i}`;
 
-    // Ajouter le nouvel élément div à la galerie
+    // Ajoute div dans gameElement pour la photo du jeu en question
+    const gameHeader = document.createElement('div');
+    gameHeader.classList.add('game_photo');
+
+    // Ajoute div dans gameElement pour les infos du jeu en question
+    const gameFooter = document.createElement('div');
+    gameFooter.classList.add('game_info');
+
+    // Ajouter les nouveaux éléments div à la galerie
     wrapperGallery.appendChild(gameElement);
+    gameElement.appendChild(gameHeader);
+    gameElement.appendChild(gameFooter);
+    console.log(gameElement.style.gridArea);
 }
 
 // catch_list_game();
